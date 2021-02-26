76f8c77440e08f78599120fa6dd625d9c27cc8ed
shravan20 <mrshravankumarb@gmail.com>
2021-02-26 22:47:51 +0530

    Adding numbers

---
+++ Add.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+    cout<<"Enter a number:";
+    cin>>a;
+    cout<<"Enter another number:";
+    cin>>b;
+    cout<<"Sum of the two numbers:"<<a+b<<".";
+
+    return 0;
+}
